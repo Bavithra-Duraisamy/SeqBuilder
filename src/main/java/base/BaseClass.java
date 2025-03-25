82e96b6a0582077ed85fd0fda8a3d4e021837792
Bavithra-Duraisamy <bavithra.arvind@gmail.com>
2025-03-25 12:16:06 +0530

    Adding gig ignore file

--- src/main/java/base/BaseClass.java
+++ src/main/java/base/BaseClass.java
@@ -6,7 +6,6 @@ import java.time.Duration;
 import java.util.Properties;
 
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
